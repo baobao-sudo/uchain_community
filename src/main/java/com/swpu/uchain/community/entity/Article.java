eee2cf7ab03e2a57d230f310fe73c9f3e27c7c8f
baobao-sudo <2581901873@qq.com>
2019-09-08 21:42:14 +0800

    git init

---
+++ src/main/java/com/swpu/uchain/community/entity/Article.java
@@ -0,0 +1,65 @@
+package com.swpu.uchain.community.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class Article implements Serializable {
+   @JsonProperty("id")
+    private Integer articleId;
+    @JsonProperty("title")
+    private String articleName;
+
+    private String articleType;
+    @JsonProperty("content")
+    private String articleContent;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getArticleName() {
+        return articleName;
+    }
+
+    public void setArticleName(String articleName) {
+        this.articleName = articleName == null ? null : articleName.trim();
+    }
+
+    public String getArticleType() {
+        return articleType;
+    }
+
+    public void setArticleType(String articleType) {
+        this.articleType = articleType == null ? null : articleType.trim();
+    }
+
+    public String getArticleContent() {
+        return articleContent;
+    }
+
+    public void setArticleContent(String articleContent) {
+        this.articleContent = articleContent == null ? null : articleContent.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", articleId=").append(articleId);
+        sb.append(", articleName=").append(articleName);
+        sb.append(", articleType=").append(articleType);
+        sb.append(", articleContent=").append(articleContent);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
